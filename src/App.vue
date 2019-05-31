181e1513175d4072086c4bad89cc284ca1ff77f0
Your Name <118735992@.con>
2019-05-31 22:22:00 +0800

    完

--- src/App.vue
+++ src/App.vue
@@ -16,10 +16,21 @@ export default {
           option:{
     tooltip: {
         trigger: 'axis',
+      
+                 formatter: function (params) {
+            console.log(params)
+            let arr = ['降水量','蒸发量','温度']
+            var res = params[0].name;
+            for (var i = 0, l = params.length; i < l; i++) {
+                res += '<br/>' +params[i].marker + arr[i] + ' : ' + params[i].value;
+            }
+           
+            return res;
+        },
         axisPointer: {
             type: 'cross',
             crossStyle: {
-                color: '#999'
+                color: "yellow"
             }
         }
     },
@@ -35,7 +46,8 @@ export default {
     // 图代表的是啥
     legend: {
         // 这里要修他的名字写死的话好对应好顺序,两个都要改!要改一起改
-        data:['zhengFaLiang','jiangShuiLiang','平均温度']
+        
+        data:['降水量','蒸发量','平均温度']
     },
     // x轴代表什么
     xAxis: [
@@ -74,29 +86,55 @@ export default {
     ],
     series: [
         {
-            name:'zhengFaLiang',
+            name:'蒸发量',
             // stack:'名字',//堆叠柱状图添加stack:"名字成统一名称即可"
+            stack:"统一",
             type:'bar',
             data:[2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
         },
         {
-            name:'jiangShuiLiang',
+            name:'蒸发量',
             type:'bar',
+            stack:"统一",
             // 添加这一 barGap属性即可实现条形统计图重叠
-            barGap: '-100%',
-            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+            // barGap: '-100%',
+            data:[2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3],
+                itemStyle: {
+                normal: {
+
+                    color: new echarts.graphic.LinearGradient(//柱状图颜色渐变
+                        0, 0,0,1,
+                        [
+                            // {offset: 0, color: '#58b9ed'},
+                            {offset: 0.5, color: 'green'},
+                            // {offset: 1, color: '#3e28d7'}
+                        ]
+                    )
+                }
+                }
         },
+        
         {
-            name:'平均温度',
-            type:'line',
+            name:'温度',
+           type:'line',
+            // stack:"统一",
             yAxisIndex: 1,
-            data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2]
+            data:[2.0, 2.2, 3.3, 4.5, 6.3, 10.2, 20.3, 23.4, 23.0, 16.5, 12.0, 6.2],
+              itemStyle : {  
+                                normal : {  
+                                    lineStyle:{  
+                                        color: "yellow"  
+                                    }  
+                                }  
+                            },  
+
         }
     ]
 },
     }
   },
   mounted() {
+      
     // 获取dom元素,加上echarts效果
     //  var myChart = echarts.init(document.getElementById('main'));
     let myChart = echarts.init(this.$refs.echarts);
